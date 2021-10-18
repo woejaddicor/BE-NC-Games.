11f3997cb035a3786d63b3ab4df82ccb902d6b0b
Joe Waddicor <joewaddicor@Joes-MacBook-Pro.local>
2021-10-18 09:13:06 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const apiRouter = require('./routers/api.router')
+const apiRouter = require('./routers/api.router');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter)
